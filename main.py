0cacc84c7af4b0df895646262ffd940ad6c1fe03
chikaskas <rivaandesai3219@gmail.com>
2025-07-03 12:02:08 +0700

    Define new_game
    in main.py

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ import random
 def safe_convert(value: any, to_type: type):
     try:
         return to_type(value)
-    except Exception as error:
+    except:
         transactions_file.close()
         save_file.close()
-        raise error
 
 transactions_file = open('transactions.txt', 'a')
 save_file = open('save.json', 'r+')
 data = save_file.read()
 
 def new_game():
-    return
+
+    data = {
+        'players': [],
+        'cash': [],
+        'last_save': False,
+        #DO LAST SAVE PROGRMMING
+    }
+
+    start_cash = safe_convert(input('Enter starting cash (int value): '), int)
+
+    #collect player names
+    for player in range (1, 17):
+        print('Enter player names below. Max players is 16. Enter "done" when finished')
+        data['players'].append(input(f'Player {player} name: '))
+        if data['players'][-1].lower() == 'done':
+            data['players'].pop(-1)
+            break
+        else:
+            data['cash'].append(start_cash)
 
 def main_loop():
     return
