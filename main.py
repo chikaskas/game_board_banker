e5fb8bfca5a4d37c985693f38db7a9d00aea14df
chikaskas <rivaandesai3219@gmail.com>
2025-07-03 15:27:21 +0700

    Define main_loop
    in main.py and edit frame.

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
-import random
+import time
+import json
 
-def safe_convert(value: any, to_type: type):
-    try:
-        return to_type(value)
-    except:
-        transactions_file.close()
-        save_file.close()
+def current_time():
+    return time.strftime("Date: %d/%m/%Y, Time: %H:%M", time.localtime())
 
 transactions_file = open('transactions.txt', 'a')
 save_file = open('save.json', 'r+')
-data = save_file.read()
+data = json.load(save_file)
+
+def update_save():
+    save_file.seek(0)
+    save_file.truncate()
+    json.dump(data, save_file)
+
+def main_loop():
+    while True:
+        print('''
+Options (Enter a number):
+1. Make transaction
+2. View leaderboard
+3. Save and exit
+''')
+        choice = input('Choice: ')
+
+        if choice == '1':
+
+            amount = int(input('Amount: '))
+            print('Accounts:')
+            print('0: Bank')
+            for number in range(len(data["players"])):
+                print(f'{number + 1}: {data["players"][number]}')
+            tranfer_from = int(input('Transfer from (enter number): '))
+            tranfer_to = int(input('Transfer to (enter number): '))
+            if tranfer_from != 0:
+                data["cash"][tranfer_from-1] -= amount
+            if tranfer_to != 0:
+                data["cash"][tranfer_to-1] += amount
+            update_save()
+
+            tranfer_from_str = 'Bank' if tranfer_from == 0 else data["players"][tranfer_from-1]
+            tranfer_to_str = 'Bank' if tranfer_to == 0 else data["players"][tranfer_to-1]
+            transactions_file.write(f'Transfered {amount} from {tranfer_from_str} to {tranfer_to_str} at {current_time()}\n')
+        
+        if choice == '2':
+            print('\nLeaderboard:')
+            sorted_cash = sorted(data["cash"], reverse=True)
+            for amount in sorted_cash:
+                print(f'{data["players"][sorted_cash.index(amount)]}: {amount}')
+
+        if choice == '3':
+            break
 
 def new_game():
 
-    data = {
-        'players': [],
-        'cash': [],
-        'last_save': False,
-        #DO LAST SAVE PROGRMMING
-    }
+    data['players'] = []
+    data['cash'] = []
+    data['last_save'] = current_time()
 
-    start_cash = safe_convert(input('Enter starting cash (int value): '), int)
+    start_cash = int(input('Enter starting cash (int value): '))
 
     #collect player names
     for player in range (1, 17):
@@ -31,11 +68,14 @@ def new_game():
             break
         else:
             data['cash'].append(start_cash)
+    
+    transactions_file.seek(0)
+    transactions_file.truncate()
 
-def main_loop():
-    return
+    update_save()
+    main_loop()
 
-if input('Continue from last save on "ADD LAST SAVE HERE" ? (y/n)')[0].lower() == 'n':
+if input(f'Continue from last save on ({data["last_save"]}) with players {data["players"]}? (y/n): ')[0].lower() == 'n':
     new_game()
 else:
     main_loop()
